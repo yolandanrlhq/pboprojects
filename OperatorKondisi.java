2b4b6d98a2e03aec9bb4c5774e570b5ca449bf9f
yolandanrlhq <yolandanurulhaq@gmail.com>
2025-09-14 20:47:21 +0700

    Initial commit

---
+++ OperatorKondisi.java
@@ -0,0 +1,8 @@
+public class OperatorKondisi {
+    public static void main(String[] args){
+        int score = 0;
+        char jawaban = 'B';
+        score = (jawaban == 'B')?100:50;
+        System.out.println("Nilai yang didapat: "+score);
+    }
+}
